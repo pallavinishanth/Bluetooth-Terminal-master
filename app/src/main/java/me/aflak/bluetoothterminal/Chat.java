5baac852519f2e25023ec7deba34902cf6dcde5f
pallavinishanth <pallavinishanth@gmail.com>
2018-07-20 10:55:56 -0400

    Final Hex to controller

--- app/src/main/java/me/aflak/bluetoothterminal/Chat.java
+++ app/src/main/java/me/aflak/bluetoothterminal/Chat.java
@@ -376,6 +376,8 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
             System.out.println(dbData.get(i));
         }
 
+        bitListtoHex(dbData);
+
         return finalHex;
 
     }
@@ -434,6 +436,21 @@ public class Chat extends AppCompatActivity implements Bluetooth.CommunicationCa
         } else {
             return (char) ('a' + (i - 10));
         }
+    }
+
+    public void bitListtoHex(List<Integer> binary){
+
+        StringBuilder hexString = new StringBuilder();
+        StringBuilder eightBits = new StringBuilder();
+        for(int i = 0; i < binary.size(); i += 8) {
+            for(int j = i; j < (i + 8) && j < binary.size(); j++) { // read next 8 bits or whatever bits are remaining
+                eightBits.append(binary.get(j)); // build 8 bit value
+            }
+            int decimal = Integer.parseInt(eightBits.toString(), 2); // get decimal value
+            hexString.append(Integer.toHexString(decimal) + " "); // add hex value to the hex string and a space for readability
+            eightBits.setLength(0); // reset so we can append the new 8 bits
+        }
+        System.out.println(hexString.toString());
     }
 }
 
